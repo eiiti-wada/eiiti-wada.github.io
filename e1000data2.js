de2f7355c90a1852bbac86696ee6d1d40f8e7112
eiiti-wada <85915429+eiiti-wada@users.noreply.github.com>
2025-11-24 10:45:10 +0900

    Add files via upload

---
+++ e1000data2.js
@@ -0,0 +1,367 @@
+var initr0=[0,0,0,0,0,0,0,0,0,
+32,24,32,28,26,32,49,32, 0, 0, 0, 0, 0, 0, // 0  1  2  3
+32,24, 1, 0, 1, 1, 0,32,12, 0, 6,17,32, 6, // 4  5  6  7
+32,10,32,12,30,32,20,32,24, 0,33,23, 0,12, // 8  9 10 11
+32,13, 0, 0,30,32,34,32,24, 0,47, 1, 0,18, //12 13 14 15
+	    32,13, 0,29,24, 1, 0,32, 0, 0, 0,26,32,55, //16 17 18 19
+	    32,24, 0,33,23, 0,24,32,20, 0, 4,17, 0,40, //20 21 22 23
+	    32,13, 0, 0,26,32, 4,32,52, 0, 6, 4, 0, 8, //24 25 26 27
+	    32, 0, 0, 0, 0, 0, 0,32,52, 0,45,52, 0, 3, //28 29 30 31
+	    32,52, 0, 6, 2, 0,56,32,23, 0, 3,13,32,28, //32 33 34 35
+	    32,10,32, 7, 9,32, 9,32,26,32, 0,48,32,54, //36 37 38 39
+	    32,13, 0, 2, 1,32,28,32, 1, 1, 0,12, 0,43, //40 41 42 43
+	    32,17,32,43,30,32, 5,32,26,32, 8,38,32,12, //44 45 46 47
+	    32,10, 0, 0, 9, 0, 1,32,24,32, 2, 1,32,28, //48 49 50 51
+	    32,26,32, 6,13, 0,57,32,24, 0,49,23, 0,57, //52 53 54 55
+	    32,26,32,40, 1, 1, 3,32,26,32,53, 0, 0,28, //56 57 58 59
+	    32, 0, 0, 0, 0, 0, 0,32,35, 0, 0,34, 0,52, //60 61 62 63
+	    32, 0, 0, 0, 0,32, 0,32, 0, 0, 0, 0, 1, 4, //64 65 66 67
+	    63,0,0,0,0,0,0,0,0];
+
+var e1000=[
+"2.71828182845904523536028747135266249775724709369995",
+"  95749669676277240766303535475945713821785251664274",
+"  27466391932003059921817413596629043572900334295260",
+"  59563073813232862794349076323382988075319525101901",
+"  15738341879307021540891499348841675092447614606680",
+"  82264800168477411853742345442437107539077744992069",
+"  55170276183860626133138458300075204493382656029760",
+"  67371132007093287091274437470472306969772093101416",
+"  92836819025515108657463772111252389784425056953696",
+"  77078544996996794686445490598793163688923009879312",
+"  77361782154249992295763514822082698951936680331825",
+"  28869398496465105820939239829488793320362509443117",
+"  30123819706841614039701983767932068328237646480429",
+"  53118023287825098194558153017567173613320698112509",
+"  96181881593041690351598888519345807273866738589422",
+"  87922849989208680582574927961048419844436346324496",
+"  84875602336248270419786232090021609902353043699418",
+"  49146314093431738143640546253152096183690888707016",
+"  76839642437814059271456354906130310720851038375051",
+"  01157477041718986106873969655212671546889570350354"];
+
+var e1000="\000\000\000\000\n\n\
+0h=82,0i=100,0p=180,0n=272,0m=300,\n\
+70:it,jlh,it,jli,p33,tllt,l2,tllt,it,jlp,0,\n\
+0h:a8n,x13r,p2n,x7r,s3n,z11r,l35,tl,p7r,a8n,jl3r,pl16n,tlm,jl,\n\
+0i:a8n,x3r,pl18n,zl,sl16n,tl20n,sl16n,tl22n,sl16n,tl24n,vl22n,ll34,\n\
+vl20n,ll35,vl18n,ll34,tl26n,p2n,x22r,x25r,s3n,z29r,ql,r33,ll33,tl,\n\
+p22r,a8n,jl18r,pl24n,al16n,vl22n,ll35,al16n,vl20n,ll35,al16n,vl18n,\n\
+ll33,alm,tlm,p2n,x46r,x48r,s3n,z52r,ql,dl26n,tl,p46r,a8n,jl42r,\n\
+pl24n,sl16n,tl18n,jl3r,\n\
+0p:a8n,x67r,q16n,p4n,x8r,x12r,s5n,z16r,pl,wl12n,tl28,ql28,tl,p8r,s8n,\n\
+jl4r,p4n,tllt,s70r,tllt,p6n,x9n,p7n,x10n,z65r,pl28,tl68,pn,x11n,\n\
+q16n,p4n,x39r,x43r,s5n,z62r,p11n,z47r,s17n,x11n,pl,wl12n,tl28,ql28,\n\
+tl,p39r,s8n,jl31r,ql68,tl28,pn,dl14n,l18,a56r,x54r,p,tllt,q2n,\n\
+vln,ql28,tl68,p1n,jl37r,p10n,s17n,jl23r,p9n,s17n,z,x9n,p8n,l12,tllt,\n\
+l2,tllt,r1,tllt,tllt,jl22r,\n\
+0n:0,10,229676,233972,197106,172332,135188,217093,245762,180224,\n\
+143360,184320,38146,254976,3814,182784,0,1,0,460,\n\
+jl70.\n\n\000\000\000\000";
+
+erato="\000\000\000\000\n\n\
+26:212998,31:212995,32:212998,\n\
+0k=88,0n=4k,0w=14n,0m=4w,0j=3m,0i=18m,0e=31m,0f=32m,\n\
+0d=37m,0b=40m,0c=46m,0a=56m,0x=262143,0y=262128,0k:0,107,\n\
+0,5664,0n:p12r,x5r,s13r,z9r,pl10r,tl,p5r,a9c,jl1r,jlm,\n\
+262143,262128,106a,512,\n\
+0w:,,,,0m:pc,t1w,p1c,f1t,pl,cl4c,zlb,alw,tlw,r1,al2k,\n\
+tl2w,ql2w,p4c,dlw,qlw,tl2w,p3w,r4,s3c,kld,a8c,xe,xf,\n\
+p3w,c45m,s5c,f2t,p9c,l,bl6c,cl,tl,p3w,a1w,t3w,jli,\n\
+p1j,a9c,jlj,plw,slk,kl40,p2c,jlj,31,1,0a,8a,346,,15,\n\
+262143,262143,512,2,\n\
+0a:0x,2y,0x,2y,0x,2y,0x,4y,0x,2y,0x,4y,0x,2y,0x,4y,\n\
+0x,6y,0x,2y,0x,6y,0x,4y,0x,2y,0x,4y,0x,6y,0x,6y,\n\
+0x,2y,0x,6y,0x,4y,0x,2y,0x,6y,0x,4y,0x,6y,0x,8y,\n\
+0x,4y,0x,2y,0x,4y,0x,2y,0x,4y,0x,8y,0x,6y,0x,4y,\n\
+0x,6y,0x,2y,0x,4y,0x,6y,0x,2y,0x,6y,0x,6y,0x,4y,\n\
+0x,2y,0x,4y,0x,6y,0x,2y,0x,6y,0x,4y,0x,2y,0x,4y,\n\
+0x,2y,0x,10y,0x,2y,0x,10y,0x,0y,jln.\n\
+88:p33,tllt,l2,tllt,jl40,jl88.0p=120,88:p26r,x8r,s27r,\n\
+z26r,l18,x9r,s28r,z23r,pl,l,kl17r,pl30r,tlo,it,jlp,\n\
+p26r,tllt,p31r,a29r,t31r,p9r,a49,jl5r,p8r,s40,jl1r,\n\
+16894,164,32,2,0,257,0p:18t,a31r,x45r,tg,nlo,k40r,\n\
+p37r,x43r,nlo,ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,\n\
+n,,pg,a33,tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,\n\
+k2,r2r,o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,\n\
+tllt,jl,jl88.\n\000\000\000\000";
+
+factorize31="\000\000\000\000\n\
+88:zl2r,jl15,p4r,jl41,156416,nl28,p12r,jl9r,p5r,t23r,\n\
+p64,kl19r,pl28,kl17r,v48,ll5,k14r,dl26r,tl28,p57,x24r,\n\
+a40,x57,,tl,jl40,38146,254976,t26.212988r,t31.212980r,\n\
+212986r,28r.\n\
+0p=116,0k=162,0c=166,0m=168,0a=256,0w=2k,32:212980m,0p:18t,\n\
+a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,ll2013,dl38r,zl14r,\n\
+a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,tg,kl43r,ll35,v34r,\n\
+rl31,jl10r,mll,n27r,e,f,k2,r2r,o18r,s,q,b,q37r,38146,\n\
+254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0m:p33,tllt,tllt,l2,tllt,plo,tlk,it,jlp,n7,r1,tllt,\n\
+0t:p2,tlw,pc,jl13r,a1w,t1w,qlk,p2,dlw,qlw,zl22r,p14r,\n\
+a49,x14r,p,kl4r,qlk,nlw,wlw,k31r,p1c,jl13r,qlk,p2,dlw,\n\
+tlk,plw,tlo,it,jlp,jl6r,plk,tlo,it,jlp,jl40,0c:0a,5a,\n\
+0a:2,1,2,2,4,2,4,2,4,6,2,6,4,2,4,6,6,2,6,4,2,6,4,6,\n\
+8,4,2,4,2,4,8,6,4,6,2,4,6,2,6,6,4,2,4,6,2,6,4,2,\n\
+4,2,10,2,10,131072,\n\
+2147483647+2147483645+2147483643+2147483641+2147483639+\n\
+2147483637+2147483635+2147483633+2147483631+2147483629+\n\
+\n\n\000\000\000\000";
+
+/*edsac="\n\
+0p=80,0q=120,0h=10,0n=170,\n\
+0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,ll2013,dl38r,zl14r,\n\
+a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,tg,kl43r,ll35,v34r,\n\
+rl31,jl10r,mll,n27r,e,f,k2,r2r,o18r,s,q,b,q37r,38146,\n\
+254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0q:a12r,tllt,x35r,plo,rl35,kl18r,nlo,rl35,p9r,jl19r,\n\
+139264,143360,j2,s,g,kllh,155648,f10r,p22r,x33r,p15r,\n\
+x14r,q22r,v13r,rl13,a17r,x27r,a,tllt,rl18,a14r,z33r,\n\
+kl21r,a,tllt,jl,\n\
+0n:,20,19,\n\
+180:n2n,a1n,tn,r18,tlo,it,jlp,pn,vn,tlo,it,jlp,pn,vn,l18,vn,\n\
+tlo,it,jlp,pn,d1n,tlo,it,jlq,pn,s2n,k1r,0,jlr."*/
+
+edsac=
+"\000\000\000\000\n0p=90,0q=140,0h=10,0n=180,0m=190,\n\
+0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,ll2013,\n\
+dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,tg,\n\
+kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,o18r,s,\n\
+q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0q:a12r,tllt,x35r,plo,rl35,kl18r,nlo,rl35,p9r,jl19r,\n\
+139264,143360,j2,s,g,kllh,155648,f10r,p22r,x33r,p15r,\n\
+x14r,q22r,v13r,rl13,a17r,x27r,a,tllt,rl18,a14r,z33r,\n\
+kl21r,a,tllt,jl,\n\
+0n:,20,21,22,0,1,tll,oll,,2,\n\
+0m:n1n,a3n,tn,p6n,tllt,l2,tllt,pn,r18,tlo,it,jlp,p7n,tllt,\n\
+pn,vn,r1,tlo,it,jlp,p7n,tllt,pn,vn,l17,vn,r1,tlo,it,jlp,\n\
+p7n,tllt,p4n,rl35,p5n,dn,al8n,tlo,it,jlq,pn,s2n,k1r,0,jlm.\n\n\
+\000\000\000\000";
+
+llt="\000\000\000\000\n\n\
+0p=116,0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,\n\
+ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,\n\
+tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,\n\
+o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\n\
+0t:0m=0r,0m:a40,x14r,p4g,x7r,s5g,z11r,p2,tl,p7r,a40,\n\
+jl3r,pg,x29r,s1g,z,p2g,x30r,s3g,z47r,p29r,sg,a30r,s2g,\n\
+a4g,x28r,x33r,a40,x36r,ql,pl,wl,tlo,q2,tl,plo,zl44r,\n\
+al,ll2013,tlo,p36r,x33r,a40,x36r,jl32r,p30r,a40,jl16r,\n\
+p29r,a40,jl12r,\n\n\
+0t:0a=0r,0a:a40,x11r,p2,tlo,pg,x13r,p1g,x14r,p2g,x18r,\n\
+s3g,z,plo,al,al,ll2013,tlo,q2,tl,p13r,a40,x13r,p14r,\n\
+a40,x14r,p18r,a40,jl9r,\n\n\
+0t:0q=0r,32:212980q,0c=380,0d=390,\n\n\
+0c:262109,2,0,1,0,2,\n\n\
+0q:plo,zlc,tl6c,it,jlp,p139r,tg,t2g,p7c,alc,kl9r,sc,\n\
+x35r,x82r,x96r,x99r,l18,t6c,ag,t1g,t3g,t4g,x58r,a6c,\n\
+a6c,t5g,s6c,s40,x91r,x92r,s6c,x95r,x100r,x128r,pl2c,l,\n\
+sl2c,tl8c,pl4c,al4c,tld,pg,a40,x47r,s1g,z50r,p2,tl,\n\
+p47r,jl42r,p7c,s5c,t7c,zl117r,it,jlm,p1g,x63r,pl,sl4c,\n\
+rl35,tlo,q2,tl,p63r,a40,x63r,x70r,plo,zl72r,al,jl60r,\n\
+pg,x84r,p91r,x80r,a40,x81r,s5g,z90r,ql,pl,rl,q2,tl,\n\
+p84r,a40,x84r,p81r,jl75r,pl8c,cl,tl,it,jla,pl,rl,tlo,\n\
+p2,ll,tl,pg,x106r,x110r,plo,zl114r,al,ll2013,tlo,q2,\n\
+tl,p106r,a40,jl102r,p7c,s3c,jl52r,pg,x121r,s95r,z127r,\n\
+pl,al2c,kl130r,p121r,a40,jl118r,pl8c,bl,zl132r,p91r,\n\
+jl133r,it,tllt,p33,tllt,l2,tllt,jl40,0d,\n\n\
+3+5+7+11+13+17+19+23+29+31+37+41+43+47+53+59+61+67+71+73+79+83+89+97+\n\
+101+103+107+109+113+127+131+137+139+149+151+157+163+167+173+179+181+191+\n\
+193+197+199+0+\n\n\000\000\000";
+
+pi1000cmp="\000\000\000\000\n\n\
+0p=196,0p:p2,tl300,p1r,a40,x1r,s8r,z40,jlr,500,jlp.\n\
+0p=196,0n=276,0m=496,0p:a7n,\n\
+x16r,p4n,x7r,s19n,z11r,p4n,tl,p7r,a7n,jl3r,pl10n,tl0,\n\
+pl4n,tl2n,sl8n,zl,q4n,p22n,x23r,x25r,s19n,z34r,ql,\n\
+dl12n,tl,sl4n,kl31r,p22n,a7n,x22n,p23r,a7n,jl19r,q4n,\n\
+p22n,x43r,a20n,x46r,x49r,x52r,s21n,z70r,ql,dl2n,tln,pl,\n\
+aln,k51r,tl,jl67r,cl14n,tl,p46r,s7n,x58r,x61r,x64r,pl,\n\
+al4n,k63r,tl,jl67r,cl14n,tl,p58r,jl54r,p43r,a7n,jl36r,\n\
+p79r,b47r,t47r,p79r,b59r,t59r,pl2n,al6n,jl14r,i,\n\
+0n:,,,,,1,,2,,1435,,80,,25,\n\
+131071,262143,425,956,57121,194,300,494,0,\n\
+0m:it,jlp,p16n,t9n,p17n,t11n,p18n,t13n,p4n,t22n,p4n,\n\
+t22n,it,jlp,0,jlm.\n\
+\n\n\000\000\000";
+
+pi1000prt="\000\000\000\000\n\n\
+0p=144,0n=222,0m=242,\n\
+0p:a8n,x67r,qn,p4n,x8r,x12r,s5n,z16r,pl,wl12n,tl28,ql28,\n\
+tl,p8r,s8n,jl4r,p5n,tllt,s17r,tllt,p6n,x9n,p7n,x10n,\n\
+z65r,pl28,tl68,pn,x11n,qn,p4n,x39r,x43r,s5n,z62r,p11n,\n\
+z47r,s74r,x11n,pl,wl12n,tl28,ql28,tl,p39r,s8n,jl31r,\n\
+ql68,tl28,pn,dl14n,l18,a56r,x54r,p,tllt,q2n,vln,ql28,\n\
+tl68,p1n,jl37r,p10n,s74r,jl23r,p9n,s74r,z,x9n,p8n,l12,\n\
+tllt,l2,tllt,r1,tllt,tllt,jl22r,\n\
+0n:0,10,229676,233972,197106,172332,135188,217093,245762,\n\
+180224,143360,184320,38146,254976,3814,182784,\n\
+0m:p33,tllt,l2,tllt,it,jlp,0,jlm.\n\n\000\000\000";
+
+/*
+edsac="\n\n\n\n0p=88,0n=120,0m=130,\n\
+0s=131072,\n\
+0p:a40,x14r,p16r,a17r,x6r,plo,l,c18r,a19r,x10r,p,tllt,p6r,s20r,z,jl3r,\n\
+as2012,ws22,es15,rs16r,ts18,ys,us,is,os,ps,a,b,c,d,e,f,\n\
+0n:0,14,0,3,oll,\n\
+0m:pln,tlo,it,jlp,p4n,tllt,pl2n,tlo,it,jlp,p4n,tllt,\n\
+pln,rl35,dl2n,tlo,it,jlp,p4n,tllt,ll35,tlo,it,jlp,0,jlr.\n\n";
+*/
+
+hexprint="\n\n\n\n0p=88,0n=120,0m=130,\n\
+0s=131072,\n\
+0p:a40,x14r,p16r,a17r,x6r,plo,l,c18r,a19r,x10r,p,tllt,p6r,s20r,z,jl3r,\n\
+as2012,ws22,es15,rs16r,ts18,ys,us,is,os,ps,a,b,c,d,e,f,\n\
+0n:1165,22136,123498,249327,\n\
+0m:pln,tlo,it,jlp,p11r,tllt,tllt,pl2n,tlo,it,jlp,oll,jlr.\n\n\n\000\000";
+
+v2test="\000\000\000\000\n\
+0p=116,0h=10,0p:a12r,tllt,x35r,plo,rl35,kl18r,nlo,rl35,\n\
+p9r,jl19r,139264,143360,j2,s,g,kllh,155648,f10r,p22r,\n\
+x33r,p15r,x14r,q22r,v13r,rl13,a17r,x27r,a,tllt,rl18,\n\
+a14r,z33r,jl21r,a,tllt,jl,\n\
+0n=16,0h=15,0f=46p,0s=50f,0w=34s,0a=16w,\n\
+0a:64,64,64,64,64,64,64,64,,,,,,,,,\n\
+0f:a40,x46r,q2,n3,d62,tl6w,tlo,it,jls,tl8w,p2,tlw,\n\
+tl4w,p47r,x25r,s48r,z32r,q49r,pl4w,wl8w,ll4,slw,tl2w,\n\
+pl4w,tlw,p,rh,al2w,tl4w,p25r,a49,jl14r,pl4w,vl8w,ll3,\n\
+slw,tl12w,nl6w,a21s,tlo,it,jls,tl10w,vl4w,ll3,tl14w,jl,\n\
+0a,0na,t,\n\
+0s:a40,x20r,plo,kl5r,nlo,s21r,tlg,vlg,tl2g,vl32r,sl30r,\n\
+vl2g,al28r,vl2g,sl26r,vl2g,al24r,vl2g,sl22r,vlg,jl,e,\n\
+51471,223880,84667,211265,41781,229407,9818,36551,1344,\n\
+71435,115,20532,\n\
+16a:p2,t3,t29r,s31r,zl28r,p33,tllt,l2,tllt,it,jlf,\n\
+pl12w,vl32r,ll23,tlo,it,jlp,p28r,tllt,pl14w,vl32r,ll23,\n\
+tlo,it,jlp,p29r,a30r,jl1r,oll,,2,16,16777,57112,jlr.\n\
+\000\000\000\000";
+
+va1test0="\000\000\000\000\n\
+88:zl2r,jl15,p4r,jl41,156416,nl28,p12r,jl9r,p5r,t23r,\n\
+p64,kl19r,pl28,kl17r,v48,ll5,k14r,dl26r,tl28,p57,x24r,\n\
+a40,x57,,tl,jl40,38146,254976,t26.212988r,t31.212980r,\n\
+212986r,28r.\n\
+0p=116,0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,\n\
+ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,\n\
+tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,\n\
+o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0h=31,0c=46p,0m=54c,0a=56c,\n\
+0c:a40,x2r,p,kl5r,a16r,x12r,x18r,x41r,l1,l2035,a4r,\n\
+x15r,pl,tl44r,alm,jl,,pla,tl,p2r,a49,jl1r,nl44r,jl38r,\n\
+q,jl27r,slm,tla,jl19r,jl26r,jl49r,vla,zl24r,wl60,jl32r,\n\
+jl58r,nl44r,ala,k26r,jl27r,jl37r,jl,p12r,jl1r,,,pla,\n\
+zl42r,jl19r,p16r,a40,x16r,z19r,jl42r,131072,0h,,,n12r,\n\
+x16r,jl19r,\n\
+0d=62c,0d:pla,zl24r,tl44c,nl24r,l1,tl26r,zl37c,pla,vla,\n\
+zl12r,wl60,jl9r,pl26r,kl19r,q,vl44c,zl19r,wl60,jl16r,q,\n\
+alm,tla,pl26r,jl4r,0,4hh,,,\n\
+0n=3,0u=9,0q=450,0q:1+1+14+2+1+10+1+2+12+\n\
+0b=28d,0b:plq,tlo,it,jlp,pr,a40,xr,s21r,z10r,jlr,p33,\n\
+tllt,l2,tllt,p21r,a20r,x21r,s22r,z40,jlr,0nn,0q,0quunn,\n\
+jl10r.0b:\n\
+0t:0k=131072,0r:pl60r,tlg,p59r,x37r,p56r,jl7r,p41r,s58r,\n\
+x41r,p37r,s58r,s56r,x37r,jl16r,p38r,s56r,x22r,x34r,x38r,\n\
+x45r,s57r,k66r,pl,zl14r,tlo,it,jlc,po,vg,tg,w62r,po,\n\
+wd,to,pk,vo,tknnuuq,nk,tk,c34r,w48r,b,n,vknnuuq,ak,\n\
+tk,c42r,j40r,p45r,s56r,x42r,x44r,x45r,s57r,k6r,jl19c,\n\
+0nn,tkq,h2,0uunnq,0,1,n41r,z68r,x16c,jl19c,p2,tlg,\n\
+plg,tlo,it,jlp,p33,tllt,l2,tllt,,\n\
+jlr.\n\000\000\000\000";
+
+va1test1="\000\000\000\000\n\
+88:zl2r,jl15,p4r,jl41,156416,nl28,p12r,jl9r,p5r,t23r,\n\
+p64,kl19r,pl28,kl17r,v48,ll5,k14r,dl26r,tl28,p57,x24r,\n\
+a40,x57,,tl,jl40,38146,254976,t26.212988r,t31.212980r,\n\
+212986r,28r.\n\
+0p=116,0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,\n\
+ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,\n\
+tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,\n\
+o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0h=31,0c=46p,0m=54c,0a=56c,\n\
+0c:a40,x2r,p,kl5r,a16r,x12r,x18r,x41r,l1,l2035,a4r,\n\
+x15r,pl,tl44r,alm,jl,,pla,tl,p2r,a49,jl1r,nl44r,jl38r,\n\
+q,jl27r,slm,tla,jl19r,jl26r,jl49r,vla,zl24r,wl60,jl32r,\n\
+jl58r,nl44r,ala,k26r,jl27r,jl37r,jl,p12r,jl1r,,,pla,\n\
+zl42r,jl19r,p16r,a40,x16r,z19r,jl42r,131072,0h,,,n12r,\n\
+x16r,jl19r,\n\
+0d=62c,0d:pla,zl24r,tl44c,nl24r,l1,tl26r,zl37c,pla,vla,\n\
+zl12r,wl60,jl9r,pl26r,kl19r,q,vl44c,zl19r,wl60,jl16r,q,\n\
+alm,tla,pl26r,jl4r,0,4hh,,,\n\
+0n=4,0u=16,0q=450,\n\
+0q:2+2+2+4+1+2+4+1+4+0+1+2+2+0+3+1+\n\
+0b=28d,0b:plq,tlo,it,jlp,pr,a40,xr,s21r,z10r,jlr,p33,\n\
+tllt,l2,tllt,p21r,a20r,x21r,s22r,z40,jlr,0nn,0q,0quunn,\n\
+jl10r.0b:\n\
+0t:0k=131072,0r:pl60r,tlg,p59r,x37r,p56r,jl7r,p41r,s58r,\n\
+x41r,p37r,s58r,s56r,x37r,jl16r,p38r,s56r,x22r,x34r,x38r,\n\
+x45r,s57r,k66r,pl,zl14r,tlo,it,jlc,po,vg,tg,w62r,po,\n\
+wd,to,pk,vo,tknnuuq,nk,tk,c34r,w48r,b,n,vknnuuq,ak,\n\
+tk,c42r,j40r,p45r,s56r,x42r,x44r,x45r,s57r,k6r,jl19c,\n\
+0nn,tkq,h2,0uunnq,0,1,n41r,z68r,x16c,jl19c,p2,tlg,\n\
+plg,tlo,it,jlp,p33,tllt,l2,tllt,,jlr.\n\n\000\000\000\000";
+
+va1test="\000\000\000\000\n\
+88:zl2r,jl15,p4r,jl41,156416,nl28,p12r,jl9r,p5r,t23r,\n\
+p64,kl19r,pl28,kl17r,v48,ll5,k14r,dl26r,tl28,p57,x24r,\n\
+a40,x57,,tl,jl40,38146,254976,t26.212988r,t31.212980r,\n\
+212986r,28r.\n\
+0p=116,0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,\n\
+ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,\n\
+tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,\n\
+o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0h=31,0c=46p,0m=54c,0a=56c,\n\
+0c:a40,x2r,p,kl5r,a16r,x12r,x18r,x41r,l1,l2035,a4r,\n\
+x15r,pl,tl44r,alm,jl,,pla,tl,p2r,a49,jl1r,nl44r,jl38r,\n\
+q,jl27r,slm,tla,jl19r,jl26r,jl49r,vla,zl24r,wl60,jl32r,\n\
+jl58r,nl44r,ala,k26r,jl27r,jl37r,jl,p12r,jl1r,,,pla,\n\
+zl42r,jl19r,p16r,a40,x16r,z19r,jl42r,131072,0h,,,n12r,\n\
+x16r,jl19r,\n\
+0d=62c,0d:pla,zl24r,tl44c,nl24r,l1,tl26r,zl37c,pla,vla,\n\
+zl12r,wl60,jl9r,pl26r,kl19r,q,vl44c,zl19r,wl60,jl16r,q,\n\
+alm,tla,pl26r,jl4r,0,4hh,,,\n\
+0n=3,0u=9,0q=450,0q:1+1+14+2+1+10+1+2+12+\n\
+0b=28d,0b:plq,tlo,it,jlp,pr,a40,xr,s21r,z10r,jlr,p33,\n\
+tllt,l2,tllt,p21r,a20r,x21r,s22r,z40,jlr,0nn,0q,0quunn,\n\
+jl10r.0b:\n\
+0t:0k=131072,0r:pl60r,tlg,p59r,x37r,p56r,jl7r,p41r,s58r,\n\
+x41r,p37r,s58r,s56r,x37r,jl16r,p38r,s56r,x22r,x34r,x38r,\n\
+x45r,s57r,k66r,pl,zl14r,tlo,it,jlc,po,vg,tg,w62r,po,\n\
+wd,to,pk,vo,tknnuuq,nk,tk,c34r,w48r,b,n,vknnuuq,ak,\n\
+tk,c42r,j40r,p45r,s56r,x42r,x44r,x45r,s57r,k6r,jl19c,\n\
+0nn,tkq,h2,0uunnq,0,1,n41r,z68r,x16c,jl19c,p2,tlg,\n\
+plg,tlo,it,jlp,p33,tllt,l2,tllt,jl40,\n\
+jlr.\n\000\000\000\000\000\000\000\000\n\
+88:zl2r,jl15,p4r,jl41,156416,nl28,p12r,jl9r,p5r,t23r,\n\
+p64,kl19r,pl28,kl17r,v48,ll5,k14r,dl26r,tl28,p57,x24r,\n\
+a40,x57,,tl,jl40,38146,254976,t26.212988r,t31.212980r,\n\
+212986r,28r.\n\
+0p=116,0p:18t,a31r,x45r,tg,nlo,k40r,p37r,x43r,nlo,\n\
+ll2013,dl38r,zl14r,a33r,5t,l18,a28r,f1t,n,tllt,pg,a33,\n\
+tg,kl43r,ll35,v34r,rl31,jl10r,mll,n27r,e,f,k2,r2r,\n\
+o18r,s,q,b,q37r,38146,254976,tlo,p32r,jl7r,n,tllt,jl,\n\
+0h=31,0c=46p,0m=54c,0a=56c,\n\
+0c:a40,x2r,p,kl5r,a16r,x12r,x18r,x41r,l1,l2035,a4r,\n\
+x15r,pl,tl44r,alm,jl,,pla,tl,p2r,a49,jl1r,nl44r,jl38r,\n\
+q,jl27r,slm,tla,jl19r,jl26r,jl49r,vla,zl24r,wl60,jl32r,\n\
+jl58r,nl44r,ala,k26r,jl27r,jl37r,jl,p12r,jl1r,,,pla,\n\
+zl42r,jl19r,p16r,a40,x16r,z19r,jl42r,131072,0h,,,n12r,\n\
+x16r,jl19r,\n\
+0d=62c,0d:pla,zl24r,tl44c,nl24r,l1,tl26r,zl37c,pla,vla,\n\
+zl12r,wl60,jl9r,pl26r,kl19r,q,vl44c,zl19r,wl60,jl16r,q,\n\
+alm,tla,pl26r,jl4r,0,4hh,,,\n\
+0n=4,0u=16,0q=450,\n\
+0q:2+2+2+4+1+2+4+1+4+0+1+2+2+0+3+1+\n\
+0b=28d,0b:plq,tlo,it,jlp,pr,a40,xr,s21r,z10r,jlr,p33,\n\
+tllt,l2,tllt,p21r,a20r,x21r,s22r,z40,jlr,0nn,0q,0quunn,\n\
+jl10r.0b:\n\
+0t:0k=131072,0r:pl60r,tlg,p59r,x37r,p56r,jl7r,p41r,s58r,\n\
+x41r,p37r,s58r,s56r,x37r,jl16r,p38r,s56r,x22r,x34r,x38r,\n\
+x45r,s57r,k66r,pl,zl14r,tlo,it,jlc,po,vg,tg,w62r,po,\n\
+wd,to,pk,vo,tknnuuq,nk,tk,c34r,w48r,b,n,vknnuuq,ak,\n\
+tk,c42r,j40r,p45r,s56r,x42r,x44r,x45r,s57r,k6r,jl19c,\n\
+0nn,tkq,h2,0uunnq,0,1,n41r,z68r,x16c,jl19c,p2,tlg,\n\
+plg,tlo,it,jlp,p33,tllt,l2,tllt,,jlr.\n\n\000\000\000\000";
+
+edsac="\000\n0x=120,0k=131072,0x:a40,x5r,p16r,x7r,s17r,z,plo,r,c18r,\n\
+a2r,x11r,p,tllt,p7r,s19r,jl3r,ak14,wk2026,ek15,rk4,tk,\n\
+yk,uk,ik,ok,pk,a,b,c,d,e,f,0t:pl10r,vl12r,tlo,it,jlx,\n\
+ll35,tlo,it,jlx,,,1,,1,jlr.";
